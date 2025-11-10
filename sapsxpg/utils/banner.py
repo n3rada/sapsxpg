2e901eea04213dea94e081d9dbfc56b457843178
n3rada <72791564+n3rada@users.noreply.github.com>
2025-11-10 20:15:59 +0100

    Add __init__.py and banner.py for version management and display

---
+++ sapsxpg/utils/banner.py
@@ -0,0 +1,15 @@
+# toboggan/utils/banner.py
+
+# Local library imports
+from sapsxpg import __version__ as version
+
+
+def display_banner() -> str:
+    return rf"""
+        _____  ___    ____
+        /  ___|/ _ \  |  _ \
+        \ `--./ /_\ \ | |_) |
+         `--. \  _  | |  __/
+        /\__/ / | | | | |   SXPG_CALL_SYSTEM
+        \____/\_| |_/ |_|             v{version}
+        """
